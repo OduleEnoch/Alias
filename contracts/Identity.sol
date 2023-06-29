b57bd3de3cd89a91348d76c95571bae97ac73680
Lawwee <lawalmohammed567@gmail.com>
2023-06-29 15:20:10 +0100

    created createIdentity() function

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -3,7 +3,38 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Identity {
-    
+contract IdentityContract {
+    using Counters for Counters.Counter;
+
+    struct Identity {
+        string name;
+        string email;
+        uint256 number;
+        // string nationality;
+        // uint256 age;
+    }
+
+    mapping(uint256 => Identity) private Identities;
+
+    Counters.Counter private _identityCounter;
+
+    constructor() {}
+
+    function createIdentity(string memory _name, string memory _email, uint256 _number) public returns (uint256) {
+        uint256 newIdentityId = _identityCounter.current();
+        _identityCounter.increment();
+
+        Identity memory newIdentity = Identity({
+            name: _name,
+            email: _email,
+            number: _number
+        });
+
+        Identities[newIdentityId] = newIdentity;
+
+        return newIdentityId;
+    }
 }
+
